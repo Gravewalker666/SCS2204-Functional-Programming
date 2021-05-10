70aaf0a0544f7f789810f3b9f21a6177331a456a
Gravewalker666 <heshanandrews99@gmail.com>
2021-05-10 10:24:51 +0000

    Add A1 Q1

--- Assignment1/Question1.scala
+++ Assignment1/Question1.scala
@@ -1,6 +1,10 @@
 object Question1 {
-  def main(args: Array[String]) {
-    print("Hello Scala")
+  def main(args: Array[String]) = {
+    print(convertCToF(35));
+  }
+
+  def convertCToF(c: Double): Double = {
+    return c*1.8 + 32;
   }
 }
 
